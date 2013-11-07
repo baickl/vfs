191e6a64eb3288f0ec506beb2ee9cc801d835152
baickl <baickl@gmail.com>
2013-11-07 19:27:02 +0800

    编写打包程序

--- tools/pack_dir.c
+++ tools/pack_dir.c
@@ -1,21 +1,60 @@
 #include "pak/pak.h"
+#include <stdio.h>
 
+static pak *g_pak = NULL;
 
-
-
-
-int pak_dir(const char* dir)
+int pak_begin( const char *path )
 {
+	pak* _pak = (pak*)malloc(sizeof(pak));
+	if( !_pak)
+		return 0;
 
+	g_pak = _pak;
+	return 1;
+}
+
+int dir_collect_fileinfo( const char *path )
+{
+	/* TODO: */
+}
+
+int dir_pack( const char *path ) 
+{
+	/* TODO: */
 }
 
 
+int pak_end( const char *path )
+{
+	if( g_pak )
+	{
+		if ( g_pak->_M_iteminfo )
+		{
+			free(g_pak->_M_iteminfo);
+			g_pak->_M_iteminfo;
+		}
 
-int main(int argc,char *argv[] )
+		free(g_pak);
+		g_pak = NULL;
+	}
+
+}
+
+int main( int argc,char *argv[] )
 {
 
+	const char* path = argv[1];
+	
+	if( 0 == pak_begin(path) )
+		return -1;
+	
+	if( 0 == dir_collect_fileinfo(path))
+		return -1;
 
-
+	if( 0 == dir_pack(path) )
+		return -1;
+	
+	pak_end(path);
 
 	return 0;
 }

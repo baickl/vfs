67c882772785ae3b8264d50282470e15b4286c24
baickl <baickl@gmail.com>
2013-11-12 20:27:49 +0800

    优化代码

--- include/pak/pak.h
+++ include/pak/pak.h
@@ -1,8 +1,19 @@
 #ifndef _PAK_H_
 #define _PAK_H_
 
-
+/* 
+ * 函数导出定义
+ * */
 #define PAK_EXTERN extern
+
+/*
+ * 常用宏定义
+ * */
+#define PAK_SAFE_FCLOSE(x)		if(x){fclose(x);x=NULL;}
+#define PAK_SAFE_FREE(x)		if(x){free((void*)x);	x=NULL;}
+
+#define PAK_CHECK_FREAD(f,b,l)	(fp ? (fread((void*)b,1,l,f)        != l ? 0 : 1) : 0 )
+#define PAK_CHECK_FWRITE(f,b,l) (fp ? (fwrite((const void*)b,1,l,f) != l ? 0 : 1) : 0 )
 
 /* 
  * MACRO DEFINE
@@ -18,6 +29,8 @@
 #define PAK_OK				1
 #define PAK_FAILED			-1
 
+
+typedef int (*dir_foreach_item_proc)(const char*fullpath,int dir);
 
 /*
  * header DEFINE
@@ -69,6 +82,8 @@ PAK_EXTERN unsigned int		pak_util_calc_crc32( void*, int );
 PAK_EXTERN int				pak_util_compress_bound( int ,int );
 PAK_EXTERN int				pak_util_compress( int,  const void*, int,void*, int );
 PAK_EXTERN int				pak_util_decompress( int, const void*, int,void*, int );
+
+PAK_EXTERN int				pak_util_dir_foreach(const char*,dir_foreach_item_proc);
 
 PAK_EXTERN int				pak_item_sort_cmp(const void*,const void*);
 PAK_EXTERN int				pak_item_search_cmp(const void*,const void*);
--- src/pak.c
+++ src/pak.c
@@ -3,7 +3,8 @@
 #include "bzip2/bzlib.h"
 #include <stdio.h>
 #include <memory.h>
-
+#include <sys/types.h>
+#include <dirent.h>
 
 int pak_item_sort_cmp(const void*a,const void*b)
 {
@@ -31,10 +32,10 @@ int pak_item_search_cmp(const void*key,const void*item)
 pak* pak_open(const char* _pakfile)
 {
 	int i;
-	pak *_pak;
-	FILE *fp;
+	pak *_pak = NULL;
+	FILE *fp = NULL;
 	pak_header header;
-	pak_iteminfo*iteminfos;
+	pak_iteminfo*iteminfos = NULL;
 	int filenamelen;
 
 	/*
@@ -48,29 +49,16 @@ pak* pak_open(const char* _pakfile)
 	 * 校验HEADER头
 	 * */
 	if( fread(&header,1,sizeof(header),fp) != sizeof(header))
-	{
-		fclose(fp);
-		return NULL;
-	}
+		goto ERROR;
 
 	if( header._M_flag != 'pakx')
-	{
-		fclose(fp);
-		return NULL;
-	}
+		goto ERROR;
 
 	if( header._M_version > PAK_VERSION )
-	{
-		fclose(fp);
-		return NULL;
-	}
+		goto ERROR;
 
 	if( header._M_count <= 0 )
-	{
-		fclose(fp);
-		return NULL;
-	}
-	
+		goto ERROR;	
 
 	/* 
 	 * 提取文件信息
@@ -78,90 +66,43 @@ pak* pak_open(const char* _pakfile)
 
 	iteminfos = (pak_iteminfo*)malloc(header._M_count*sizeof(pak_iteminfo));
 	if( !iteminfos )
-	{
-		fclose(fp);
-		return NULL;
-	}
+		goto ERROR;	
 
 	for( i = 0; i<header._M_count; ++i )
 	{
 
-
 		if( fread(&iteminfos[i]._M_offset,1,sizeof(iteminfos[i]._M_offset),fp) != sizeof(iteminfos[i]._M_offset))
-		{
-			fclose(fp);
-			free(iteminfos);
-			return NULL;
-		}
-
+			goto ERROR;
 		iteminfos[i]._M_offset += header._M_offset; 
-
+		
 		if( fread(&iteminfos[i]._M_size,1,sizeof(iteminfos[i]._M_size),fp) != sizeof(iteminfos[i]._M_size))
-		{
-			fclose(fp);
-			free(iteminfos);
-			return NULL;
-		}
+			goto ERROR;
 
 		if( fread(&iteminfos[i]._M_crc32,1,sizeof(iteminfos[i]._M_crc32),fp) != sizeof(iteminfos[i]._M_size))
-		{
-			fclose(fp);
-			free(iteminfos);
-			return NULL;
-		}
+			goto ERROR;	
 
 		if( fread(&iteminfos[i]._M_compress_type,1,sizeof(iteminfos[i]._M_compress_type),fp) != sizeof(iteminfos[i]._M_compress_type))
-		{
-			fclose(fp);
-			free(iteminfos);
-			return NULL;
-		}
+			goto ERROR;
 
 		if( fread(&iteminfos[i]._M_compress_size,1,sizeof(iteminfos[i]._M_compress_size),fp) != sizeof(iteminfos[i]._M_compress_size))
-		{
-			fclose(fp);
-			free(iteminfos);
-			return NULL;
-		}
-
+			goto ERROR;	
 
 		if( fread(&iteminfos[i]._M_compress_crc32,1,sizeof(iteminfos[i]._M_compress_crc32),fp) != sizeof(iteminfos[i]._M_compress_crc32))
-		{
-			fclose(fp);
-			free(iteminfos);
-			return NULL;
-		}
+			goto ERROR;
 
 		filenamelen = 0;
 		if( fread(&filenamelen,1,sizeof(filenamelen),fp) != sizeof(filenamelen))
-		{
-			fclose(fp);
-			free(iteminfos);
-			return NULL;
-		}
+			goto ERROR;
 
 		if( filenamelen <= 0 || filenamelen >= PAK_MAX_FILENAME )
-		{
-			fclose(fp);
-			free(iteminfos);
-			return NULL;
-		}
+			goto ERROR;
 
 		memset(iteminfos[i]._M_filename,0,sizeof(iteminfos[i]._M_filename));
 		if( fread(iteminfos[i]._M_filename,1,filenamelen,fp) != filenamelen )
-		{
-			fclose(fp);
-			free(iteminfos);
-			return NULL;
-		}
+			goto ERROR;
 	}
 
-
-	if( fp )
-	{
-		fclose(fp);
-		fp = NULL;
-	}
+	PAK_SAFE_FCLOSE(fp);
 
 
 	/* 
@@ -169,25 +110,24 @@ pak* pak_open(const char* _pakfile)
 	 * */
 	_pak = (pak*)malloc(sizeof(pak));
 	if( !_pak )
-	{
-		free(iteminfos);
-		return NULL;
-	}
+		goto ERROR;
+	
 	memset(_pak->_M_filename,0,sizeof(_pak->_M_filename));
 	memset(&_pak->_M_header,0,sizeof(_pak->_M_header));
 	strcpy(_pak->_M_filename,_pakfile);
 	memcpy(&_pak->_M_header,&header,sizeof(header));
 	_pak->_M_iteminfos = iteminfos;
 
-	printf("successed:open_pak count=%d,offset=%d\n",
-			_pak->_M_header._M_count,
-			_pak->_M_header._M_offset);
-
 	/*
 	 * 排序
 	 * */
 	qsort((void*)_pak->_M_iteminfos,_pak->_M_header._M_count,sizeof(pak_iteminfo),pak_item_sort_cmp);
 	return _pak;
+
+ERROR:
+	PAK_SAFE_FCLOSE(fp);
+	PAK_SAFE_FREE(iteminfos);
+	return NULL;
 }
 
 void pak_close(pak* _pak)
@@ -256,6 +196,63 @@ int pak_util_decompress(int compresstype,const void*src,int srcsize,void*dst,int
 	}
 }
 
+int pak_util_dir_foreach(const char* path,dir_foreach_item_proc proc)
+{
+	DIR* dir;
+	struct dirent *entry = NULL;
+
+	char find_full[PAK_MAX_FILENAME+1];
+	char path_temp[PAK_MAX_FILENAME+1];
+
+	strcpy(find_full,path);
+	
+	dir = opendir(path);
+	if( NULL == dir )
+		return 0;
+
+	if( NULL == proc )
+		return 0;
+
+	while( (entry=readdir(dir)) != NULL )
+	{
+
+		if( entry->d_type & DT_DIR )
+		{
+			if( strcmp(entry->d_name,".")  == 0 ||
+				strcmp(entry->d_name,"..") == 0 )
+				continue;
+			memset(path_temp,0,sizeof(path_temp));
+			strcpy(path_temp,path);
+			strcat(path_temp,"/");
+			strcat(path_temp,entry->d_name);
+
+			if( proc )
+				proc(path_temp,1);
+
+			if( 0 == pak_util_dir_foreach(path_temp,proc) )
+				goto ERROR;
+		}		
+		else
+		{
+			memset(path_temp,0,sizeof(path_temp));
+			strcpy(path_temp,path);
+			strcat(path_temp,"/");
+			strcat(path_temp,entry->d_name);
+
+			if( proc )
+				proc(path_temp,0);
+		}
+	}
+
+	closedir(dir);
+	return 1;
+
+ERROR:
+	closedir(dir);
+	return 0;
+
+}
+
 int pak_item_getcount(pak* _pak)
 {
 	if( !_pak)
@@ -293,9 +290,9 @@ int  pak_item_locate(pak*_pak,const char* _file)
 
 int pak_item_unpack_index( pak* _pak,int _index,void *_buf,int _bufsize)
 {
-
-	pak_iteminfo* iteminfo;
-	FILE* fp;
+	pak_iteminfo* iteminfo = NULL;
+	FILE* fp = NULL;
+	void* compress_buf = NULL;
 
 	if( !_pak || !_buf )
 		return 0;
@@ -316,81 +313,56 @@ int pak_item_unpack_index( pak* _pak,int _index,void *_buf,int _bufsize)
 		return 0;
 
 	if( fseek(fp,iteminfo->_M_offset,SEEK_SET) != 0)
-	{
-		fclose(fp);
-		return 0;
-	}
+		goto ERROR;
 
 	if( iteminfo->_M_compress_type == PAK_COMPRESS_NONE)
 	{
 		if( fread(_buf,1,iteminfo->_M_size,fp) != iteminfo->_M_size)
-		{
-			fclose(fp);
-			return 0;
-		}
+			goto ERROR;
 
-		fclose(fp);
-		fp = 0;
-
+		PAK_SAFE_FCLOSE(fp);
+		
 		/*
 		 * 校验数据是否正确
 		 * */
 		unsigned int crc32 = pak_util_calc_crc32(_buf,iteminfo->_M_size);
 		if( crc32 != iteminfo->_M_crc32 )
-		{
 			return 0;
-		}
 
 		return iteminfo->_M_size;
 	}
 	else if( iteminfo->_M_compress_type == PAK_COMPRESS_BZIP2 )
 	{
 
-		void* compress_buf = malloc(iteminfo->_M_compress_size);
+		compress_buf = malloc(iteminfo->_M_compress_size);
 		if( !compress_buf )
-		{
-			fclose(fp);
-			return 0;
-		}
+			goto ERROR;
 
 		if( fread(compress_buf,1,iteminfo->_M_compress_size,fp) != iteminfo->_M_compress_size)
-		{
-			free(compress_buf);
-			fclose(fp);
-			return 0;
-		}
+			goto ERROR;
 
 		if( pak_util_calc_crc32(compress_buf,iteminfo->_M_compress_size) != iteminfo->_M_compress_crc32)
-		{
-			free(compress_buf);
-			fclose(fp);
-			return 0;
-		}
+			goto ERROR;
 
 		if( pak_util_decompress( iteminfo->_M_compress_type,
 								 compress_buf,iteminfo->_M_compress_size,
 								 _buf,_bufsize) != iteminfo->_M_size  )
-		{
-			free(compress_buf);
-			fclose(fp);
-			return 0;
-		}
+			goto ERROR;
 
-		free(compress_buf);
-		fclose(fp);
+		PAK_SAFE_FREE(compress_buf);
+		PAK_SAFE_FCLOSE(fp);
 
 		if( pak_util_calc_crc32(_buf,iteminfo->_M_size) != iteminfo->_M_crc32) 
-		{
 			return 0;
-		}
 		
 		return iteminfo->_M_size;
 	}
-	else
-	{
-		fclose(fp);
-		return 0;
-	}
+	
+ERROR:
+	PAK_SAFE_FREE(compress_buf);
+	PAK_SAFE_FCLOSE(fp);
+
+	return 0;
 }
 
 int pak_item_unpack_filename(pak*_pak,const char*_file,void*_buf,int _bufsize)

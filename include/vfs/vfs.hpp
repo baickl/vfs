3e6306364e88adff25a8c98d8bf3c919d6bb71d8
baickl <baickl@gmail.com>
2013-11-21 01:04:10 +0800

    添加C＋＋包含用的头文件

---
+++ include/vfs/vfs.hpp
@@ -0,0 +1,17 @@
+#ifndef _VFS_VFS_HPP_
+#define _VFS_VFS_HPP_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include "vfs.h"
+#include "util.h"
+#include "pak.h"
+#include "common.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
